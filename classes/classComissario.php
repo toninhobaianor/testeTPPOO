0f9f6b102c894bf95daca7b180fea2f20d4f5cc0
Antonio Alberto <85882536+toninhobaianor@users.noreply.github.com>
2023-05-27 15:57:35 -0300

    Delete classes directory

--- classes/classComissario.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../libs/global.php");
-
-class Comissario extends Tripulante
-{
-    private int $tipoTripulante = COMISSARIO;
-
-    static $local_filename = "comissarios.txt";
-}
