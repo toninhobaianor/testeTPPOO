77ea51631275c70cb81aae00c027410f9ab0259b
toninhobaianor <85882536+toninhobaianor@users.noreply.github.com>
2023-05-27 19:51:02 +0000

    melhorando o site
    .

--- classes/site.php
+++ classes/site.php
@@ -1,91 +1,58 @@
 <?php
 
 include_once("../libs/global.php");
+include_once("sistema/file/sistemaCliente.php")
 
 $sair = 0;
-$valido = 0;
-$temviaje = 0;
-  $dia = 23;
-  $mes = 08;
-  $aeroportoOrigem = "bh";
-  $aeroportoDestino = "sp";
-  $companhiaAerea = "gol";
-  private Aeronave $aeronave;
 
-  $voo = 1;
-  $milhasViagem = 0;
-  $valorViagem = 275,51;
-  $valorFranquiaBagagem = 26,90;
 
-$viaje = new Viagem();
 while ($sair == 0) {
+    $escolha = 0;
     print_r("------ BEM VINDO AO SITE DAS COMPAHIAS AEREAS ------\r\n");
-  
-    //$opcao = (string)readline("Digite seu email: ");
-    //validalogin($opcao); 
-    // essa função poderia retornar um cliente e ser colocada em uma variavel
 
-    print_r("agora oque o senhor(a) '' deseja fazer \r\n");
-    print_r("C - comprar passagem\r\n");
-    print_r("A - alterar passagem\r\n");
-    print_r("D - cancelar passagem\r\n");
-    print_r("S - sair do site\r\n");
-    $op = (string)readline("Digite uma opcao: ");
-    if($op == "C"){
-        print_r("nos conte um pouco sobre a viagem que deseja fazer\r\n");
-        $local_parti = (string)readline("Digite de onde vai sai(sigla do estado): ");
-        $local_cheg = (string)readline("Digite para onde vai (sigla do estado): ");
-        $dia = (int)readline("Digite o dia que deseja sair: ");
-        $mes = (int)readline("Digite o mes que deseja sair: ");
-        print_r("Index - Aeroporto de partdida - Destino - dia - Horario - companiaaeria\r\n");
-        procurar_viajem($local_parti,$local_cheg,$dia,$mes);
-        if($temviaje >= 1){
-            print_r("encontramos estas viajens\r\n");
-            $via = (int)readline("Digite em qual delas deseja viajar: ");
-            escolher_assento($via);
-            $assento = (string)readline("Digite em qual assento deseja viajar: ");
-            print_r("quem vai viajar(se for voce digite 1 se for outra pessoa digite 2)\r\n");
-            $esc = (int)readline("Digite aqui: ");
-            if($esc == 1){
-              $email = (string)readline("qual o seu email:");
-              validalogin($email);
-              if($valido == 1){
-                
-              }
-              else{
-                $cliente = fazer_cadastro();
-                $passageiro = criar_passageiro($cliente);
-                
-              }
-              //pergunta se e cliente
-              //cria o passageiro a partir do cliente
-              comprar_passagem($via,$assento);
-            }
-            else{
-              //pergunta se e cliente
-              //cria um passageiro com algumas perguntas
-              comprar_passagem($via,$assento);
-            }
+
+    print_r("---CLIENTE--- \r\n");
+    print_r(++$escolha . " - Cadastra Cliente\r\n");
+    print_r(++$escolha . " - Ver Clientes\r\n");
+    print_r(++$escolha . " - Editar Cliente\r\n");
+
+    $opcao = (string)readline("Digite uma opcao: ");
+
+    $escolha = 0;
+
+    switch($opcao){
+
+        case ++$escolha:
+            print_r("Cadastramento de Clientes\r\n");
+            print_r("\n\n");
+            cadastra_Cliente();
+            break;
             
-        }
-        else{
-    
-        }
-    }
-    if($op == "A"){
-        
-    }
-    if($op == "D"){
-        //saber se e cliente
-        //assesar o historico de compras
-      //retirar o objeto procurado e destrui-lo
-    }
-    if($op == "S"){
-        print_r("tenha um bom dia!!!");
-        $sair++;
-    }
+        case ++$escolha:
+            print_r("Ver Clientes\r\n");
+            print_r("\n\n");
+            ver_Clientes();
+            break;
 
+        case ++$escolha:
+            print_r("Editar Aeroporto\r\n");
+            print_r("\n\n");
+            editar_Cliente();
+            break;
+
+        case -1:
+            print_r("Saindo do sistema...\r\n");
+            $sair = 1;
+            break;
+    
+        default:
+            print_r("Opcao errada!!\r\n");
+            print_r("\n\n");
+            break;
+    }
 }
+
+/*
 function criar_passageiro(Cliente $cliente){
   $tipo= False;
   $nome = $cliente->getNome();
@@ -154,3 +121,4 @@ function comprar_passagem(int $index,string $assento){
         }
     }
 }
+*/
