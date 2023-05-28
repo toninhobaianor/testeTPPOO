b1be532977c56c1a88c930aaf5371f133aa9e517
Antonio Alberto <85882536+toninhobaianor@users.noreply.github.com>
2023-05-28 12:30:14 -0300

    Add files via upload

---
+++ classes/sistema/files/sistemaPassagem.php
@@ -0,0 +1,21 @@
+<?php
+
+function criar_Passagem(){
+
+}
+
+function mostrar_Passagems(array $passagens){
+
+}
+
+function ver_passagens(){
+
+}
+
+function altera_Passagem(){
+
+}
+
+function fazer_checkin(){
+    
+}
